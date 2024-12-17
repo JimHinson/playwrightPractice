d4aa04679da3a56484b4dafc64263975865d762f
Jim Hinson <jim.hinson@live.com>
2024-12-17 10:21:03 -0500

    Practice

--- tests/contactListApp.spec.ts
+++ tests/contactListApp.spec.ts
@@ -1,5 +1,6 @@
 // @ts-check
 import { test, expect, type Page } from '@playwright/test';
+import { assert } from 'console';
 
 
 
@@ -15,18 +16,35 @@ test.describe('Sign up', () => {
     // await expect(page).toHaveTitle('/.*Contact');
   });
 
-  test('email', async ({ page }) => {
+  
+  test.only('email', async ({ page }) => {
     await page.goto('https://thinking-tester-contact-list.herokuapp.com/');
 
     // Click the get started link.
-      await page.getByRole('textbox', { name: 'email'}).fill('Jim Hinson')
-      await page.getByRole('textbox', { name: 'password'}).fill('')
-      await page.getByRole('button', {name: 'Submit'}).click;
+      await page.getByRole('textbox', { name: 'email'}).fill('Jim Hinson');
+      await page.getByRole('textbox', { name: 'password'}).fill('');
+      // await page.getByTestId('submit')
+      await page.getByRole('button', {name: 'Submit'}).click();
 
       await expect(page).toHaveTitle('Contact List App');
+      // await page.getByTestId('error').click();
+      
+      //Incorrect username or password
+      await expect(page.getByText('Incorrect username or password')).toBeVisible();
 
-    // await page.getByRole('textbox', {   'email'}).click
-    // await page.getByTestId('email').type('Jim Hinson')
+      for (let i = 0; i < 5; i++) {
+        console.log("i: " + i);
+      }
+      
+      const fruits = ['apple', 'banana', 'cherry'];
+      fruits.forEach ((fruit, index /*, array */) => {
+        console.log(`Index: ${index}, Fruit: ${fruit}`);
+        console.log("Index: " + index + "; Fruit: " + fruit)
+        // console.log("array: " + array)
+      });
+      // fruits.forEach()
+
+
 
     // Expects page to have a heading with the name of Installation.
     // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
--- tests/start.spec.ts
+++ tests/start.spec.ts
@@ -1,27 +1,33 @@
 // @ts-check
 import { test, expect, type Page } from '@playwright/test';
 
+const testURL = "https://the-internet.herokuapp.com/windows";
 
-//test.describe('Mark all as completed', () => {
+test.beforeEach(async ({ page }) => {
+    await page.goto(testURL);
+  });
 
 test.describe('start', () => {
 
     test('Open new tab', async ({page}) => {
-        await page.goto('https://the-internet.herokuapp.com/windows');
+        // await page.goto(testURL);
 
         const newPagePromise = page.waitForEvent('popup');
 
         await page.getByRole('link', {name: 'Click Here'}).click();
         
-        const newPage = await newPagePromise; 
+        // const newPage = await newPagePromise; 
 
-        // await expect(newPage).toHaveTitle(/New Window/);
+        await expect(page).toHaveTitle("The Internet");
     });
 
 
     test('should allow me to mark all items as completed', async ({ page }) => {
+        // const newPagePromise = page.waitForEvent('popup');
+
+        await page.getByRole('link', {name: 'Click Here'}).click();
         // Complete all todos.
-        // await page.getByLabel('Mark all as complete').check();
+        await page.getByLabel('Mark all as complete').check();
 
         // Ensure all todos have 'completed' class.
         // await expect(page.getByTestId('todo-item')).toHaveClass(['completed', 'completed', 'completed']);
